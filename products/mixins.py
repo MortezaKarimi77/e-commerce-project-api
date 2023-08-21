234682f2385a61c5d0e1f087fffbdc7cb045cd6c
Morteza Karimi <itismortezakarimi@gmail.com>
2023-08-21 16:03:10 +0330

    change get_queryset method on ProductListCreate to be dynamic based on user

--- products/mixins.py
+++ products/mixins.py
@@ -6,13 +6,21 @@ from .models import Product
 
 
 class ProductAPIViewMixin:
+    model = Product
     queryset = Product.objects.all()
     permission_classes = (IsAdminOrReadOnly,)
 
     def get_queryset(self):
-        queryset = super().get_queryset()
+        user = self.request.user
+
+        if user.is_staff:
+            queryset = super().get_queryset()
+            cache_key = "all_products_queryset"
+        else:
+            queryset = self.model.objects.get_visible_products()
+            cache_key = "visible_products_queryset"
 
         cached_queryset = cache.get_or_set(
-            key="products_queryset", default=queryset, timeout=None
+            key=cache_key, default=queryset, timeout=None
         )
         return cached_queryset
